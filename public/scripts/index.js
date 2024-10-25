773f72dfd420d84ef104d4f5ae39de54315fc170
Ankan Layek <ankanlayek2004@gmail.com>
2024-10-25 22:07:09 +0530

    kmph & map view fixed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,7 +5,7 @@ const socket = io();
 if(navigator.geolocation){
     navigator.geolocation.watchPosition((position) => {
         const {latitude, longitude, accuracy, speed} = position.coords;
-        // console.log(latitude, longitude, accuracy/1000);
+        console.log(latitude, longitude, accuracy/1000);
         socket.emit("send-location", { latitude, longitude, speed });
     },
     (error) => {
@@ -24,11 +24,42 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "Ankan Layek"
 }).addTo(map);
 
+const centerCircle = document.createElement("div");
+centerCircle.id = "centerCircle";
+
+const centerIcon = document.createElement("i");
+centerIcon.classList.add("fa-solid", "fa-location-crosshairs");
+centerIcon.id = "centerIcon";
+
+centerCircle.appendChild(centerIcon);
+document.querySelector("#map").appendChild(centerCircle);
+
 const markers = {};
+let autoCenter = true;
+
+map.on("movestart", ()=> {
+    console.log("auto center disabled");
+    autoCenter = false;
+})
 
 socket.on("receive-location", (data) => {
     let {id, latitude, longitude, speed} = data;
-    map.setView([latitude, longitude], 13);
+
+    if(autoCenter){
+        map.setView([latitude, longitude], 13);
+        autoCenter = true
+    }
+
+    const centerMap = () => {
+        console.log("Map centered");
+        map.setView([latitude, longitude], 13);
+        autoCenter = true;
+    }
+    centerIcon.addEventListener("click", () => {
+        centerMap();
+        setTimeout(centerMap, 10);
+    })
+
     if(markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
     }
@@ -36,11 +67,18 @@ socket.on("receive-location", (data) => {
         markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
 
+
+    let kmph
+
     if(speed == null){
-        speed = "null"
+        kmph = "null"
+    } else{
+        kmph = 3.6*speed;
+        kmph = Math.round(kmph*100)/100
     }
 
-    speedLine.innerText = `${speed}`;
+    speedLine.innerText = `${kmph}`;
+    console.log(autoCenter)
 });
 
 socket.on("user-disconnected", (id) => {
@@ -48,4 +86,4 @@ socket.on("user-disconnected", (id) => {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
-})
+});
